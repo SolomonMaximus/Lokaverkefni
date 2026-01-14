627f9539a2057129847e314bada4b576548e9bba
Solon <asacornette@gmail.com>
2026-01-14 23:03:02 +0000

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,70 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
-import RecipesInfo from "./pages/RecipesInfo";
-import NotFound from "./pages/NotFound";
 export default function App() {
-  return <h1>Recipesite</h1>;
+  const [page, setPage] = useState("home");
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function goHome() {
+    setPage("home");
+    setMenuOpen(false);
+  }
+
+  function goRecipes() {
+    setPage("recipes");
+    setMenuOpen(false);
+  }
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
+  return (
+    <div className="page">
+      <div className="header">
+        <div
+          className="header-left"
+          onClick={toggleMenu}
+          style={{ cursor: "pointer" }}
+        >
+          ☰
+        </div>
+
+        <div
+          className="header-title"
+          onClick={goHome}
+          style={{ cursor: "pointer" }}
+        >
+          Bon appétit
+        </div>
+      </div>
+
+      {menuOpen && (
+        <div className="menu">
+          <p onClick={goHome}>Home</p>
+          <p onClick={goRecipes}>Recipes</p>
+        </div>
+      )}
+
+      <div className="content">
+        {page === "home" && (
+          <>
+            <h1>Hello world</h1>
+            <p>Þetta er síðan mín</p>
+          </>
+        )}
+
+        {page === "recipes" && (
+          <>
+            <h1>Recipes</h1>
+            <p>Setja recipes hér.</p>
+          </>
+        )}
+      </div>
+
+      <div className="footer">
+        <p> Bon appétit</p>
+      </div>
+    </div>
+  );
 }
