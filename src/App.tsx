08872f8adb0653d096cb4745993db847160a2951
Solon <asacornette@gmail.com>
2026-01-16 20:31:57 +0000

    third

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ export default function App() {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const recipes = [
+    { id: 1, title: "Pasta", description: "chicken pasta ,very good" },
+    { id: 2, title: "Soup", description: "warm and good for you" },
+    { id: 3, title: "Cake", description: "tasty cake" },
+    { id: 4, title: "Salad", description: "Healty for you" },
+  ];
+
+  const [selectedRecipe, setSelectedRecipe] = useState<null | {
+    id: Number;
+    title: string;
+    description: String;
+  }>(null);
+
   function goHome() {
     setPage("home");
     setMenuOpen(false);
@@ -57,7 +70,44 @@ export default function App() {
         {page === "recipes" && (
           <>
             <h1>Recipes</h1>
-            <p>Setja recipes hér.</p>
+
+            {selectedRecipe === null ? (
+              <div className="card-grid">
+                {recipes.map((recipe) => (
+                  <div key={recipe.id} className="card">
+                    <div
+                      className="card-image"
+                      onClick={() => setSelectedRecipe(recipe)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      Image
+                    </div>
+
+                    <h3
+                      onClick={() => setSelectedRecipe(recipe)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {recipe.title}
+                    </h3>
+
+                    <p>{recipe.description}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="detail">
+                <div className="detail-image">Image</div>
+                <h2>{selectedRecipe.title}</h2>
+                <p>(Hérna kemur api datað)</p>
+
+                <p
+                  onClick={() => setSelectedRecipe(null)}
+                  style={{ cursor: "pointer" }}
+                >
+                  Back to recipes
+                </p>
+              </div>
+            )}
           </>
         )}
       </div>
